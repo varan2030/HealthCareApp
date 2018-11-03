2e4e9c28d9dca8b6ae392213db247f6cf413eb08
varan2030 <varan2030@>
2018-11-02 20:23:50 -0400

    HealthCare app create

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Container } from 'reactstrap';
+
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
+  render() {
+    return (
+      <Container>
+      <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+        <DropdownToggle caret>
+          Dropdown
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem>Another Action</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      </Container>
+    );
+  }
+}
